3c7881b7b3179cbea091686f3655c49cd776054f
6GrandDad <sleeptherank@gmail.com>
2026-05-29 23:40:19 -0400

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -79,11 +79,3 @@ if whitelist and whitelist[userId] then
     end
 
     return loadstring(downloadFile('newvape/main.lua'), 'main')()
-else
-
-    game.StarterGui:SetCore("SendNotification", {
-        Title = "Fuck nah u thought",
-        Text = "ur not whitelisted nn lmao",
-        Duration = 2
-    })
-end
